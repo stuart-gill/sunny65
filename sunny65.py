c39bfb46b2dd03099b1033a9244ed03472134082
Stuart Gill <stuart.a.gill@gmail.com>
2020-06-01 20:42:34 -0700

    now able to use filtered database desinations to send to google maps distance matrix, and use those travel filtered results to request weather resultsgit add .

--- sunny65.py
+++ sunny65.py
@@ -6,9 +6,10 @@
 # 3: output destinations, weather + distance data
 
 
-# TODO: make distance matrix its own file and function. Input: one origin and list of destinations. Output: list of distances
+# TODO: make distance matrix its own file and function. Input: one origin and list of destinations. Output: list of distances (or distance / name dicts?)
 # TODO: make weather api its own file and function. Input: lat, long. Output: list of weather dictionaries for that location
 # TODO: use lat long of geocoded origin to return destinations from database within range. own function/file
+# 
 
 
 import urllib.request, urllib.parse, urllib.error
@@ -30,12 +31,12 @@ weather_service_url = 'https://api.weather.gov/points/'
 
 # Read JSON to build destinations parameter
 # Should be able to extend this to reading from a database
-str_data = open('sunny65.json').read()
-destinations = json.loads(str_data)
+# str_data = open('sunny65.json').read()
+# destinations = json.loads(str_data)
 
-destinations_parm = ''
-for loc in destinations:
-  destinations_parm += str(loc["lat"]) + "," + str(loc["lng"]) + '|'
+# destinations_parm = ''
+# for loc in destinations:
+#   destinations_parm += str(loc["lat"]) + "," + str(loc["lng"]) + '|'
 
 
 # Ignore SSL certificate errors
@@ -52,6 +53,9 @@ while True:
     est_miles = travel*45  # super rough guess of how far you could go in an hour
     distance_filtered_locs = calc_distance(lat,lng,est_miles)
     print(distance_filtered_locs)
+    destinations_parm = ''
+    for loc in distance_filtered_locs:
+      destinations_parm += str(loc[2]) + "," + str(loc[3]) + '|'
     parms = dict()
     parms['origins'] = address
     parms['destinations']= destinations_parm
@@ -74,34 +78,36 @@ while True:
         print(data)
         continue
 
-    # print(json.dumps(js, indent=4))
+    print(json.dumps(js, indent=4))
 
     # travel = float(input('How long are you willing to travel in hours? '))
     travel_seconds = travel * 3600
-    acceptable_results = []
+    acceptable_indices = []
 
     user_min = int(input("What's your minimum acceptable temperature? "))
     user_max = int(input("What's your maximum acceptable temperature? "))
 
     elements = js["rows"][0]["elements"]
 
+    # get list of indexes that pass travel duration test
     for i in range(len(elements)):
-      if elements[i]["duration"]["value"] < travel_seconds:
-        acceptable_results.append(i)
+      if elements[i]["status"] == "OK" and elements[i]["duration"]["value"] < travel_seconds:
+        acceptable_indices.append(i)
+        
 
-    # print names of acceptable cities ('result' is the index in the list of destinations... should convert to IDs when using database)
+    # print names of acceptable cities ( index in the list of destinations... should convert to IDs when using database)
     print("Destinations within acceptable drive time: ")
-    for result in acceptable_results:
-      print(destinations[result]["name"])
+    for i in acceptable_indices:
+      print(distance_filtered_locs[i][1])
 
-
+    destinations = []
     # GET WEATHER FROM WEATHER.GOV 
-    for result in acceptable_results:
+    for i in acceptable_indices:
       # Add weather key
-      destinations[result]["weather"]= []
+      destinations.append({"id":distance_filtered_locs[i][0], "name":distance_filtered_locs[i][1], "lat":distance_filtered_locs[i][2], "lng":distance_filtered_locs[i][3], "weather":[]})
       
       # Get forecast API URL using lat and long
-      url = weather_service_url + str(destinations[result]["lat"]) + "," + str(destinations[result]["lng"])# + "/forecast"
+      url = weather_service_url + str(distance_filtered_locs[i][2]) + "," + str(distance_filtered_locs[i][3])# + "/forecast"
       
       try: 
         uh = urllib.request.urlopen(url, context=ctx)
@@ -138,14 +144,14 @@ while True:
       for period in js["properties"]["periods"]:
         if period["isDaytime"]:
           if period["temperature"] > user_min and period["temperature"] < user_max:
-            destinations[result]["weather"].append(period)
+            destinations[-1]["weather"].append(period)
 
     # output acceptable weather windows at acceptable destinations
-    for result in acceptable_results:
-      if len(destinations[result]["weather"]):
+    for destination in destinations:
+      if len(destination["weather"]):
         print('============================= \n')
-        print("Destination: ", destinations[result]["name"])
-        for day in destinations[result]["weather"]:
+        print("Destination: ", destination["name"])
+        for day in destination["weather"]:
           print("Day: ", day["name"])
           print("Temperature: ", str(day["temperature"]))
           print("Weather: ", day["shortForecast"])
