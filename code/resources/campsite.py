11d10c3b68d390b63405c4d4e6d87d9c74182f76
Stuart Gill <stuart.a.gill@gmail.com>
2020-07-13 19:52:39 -0700

    add campsite resource

---
+++ code/resources/campsite.py
@@ -0,0 +1,94 @@
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
+from models.campsite import CampsiteModel
+
+
+# resources used to map endpoints (like get, post) to /campsite/name or whatever
+# anything not called by an API directly shouldn't be a resource but a model
+
+# resources use models to interpret data pulled from database... models stand on their own
+
+
+class Campsite(Resource):
+
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        "lat", type=float)
+    parser.add_argument(
+        "lng", type=float)
+    parser.add_argument(
+        "weather_url", type=str)
+
+
+    # @jwt_required()
+    def get(self, name):
+        campsite = CampsiteModel.find_by_name(name)
+        if campsite:
+            return campsite.json()
+        return {"message": "campsite not found"}, 404
+
+    def post(self, name):
+        if CampsiteModel.find_by_name(name):
+            return {"message": f"an campsite with name {name} already exists"}, 400
+
+        data = Campsite.parser.parse_args()
+        campsite = CampsiteModel(name, data["lat"], data["lng"], data["weather_url"])
+
+        try:
+            campsite.upsert()
+        except:
+            return {"message": "an error occured while trying to post the campsite"}, 500
+
+        # note: we always have to return json
+        return campsite.json(), 201
+
+    # @jwt_required()
+    def delete(self, name):
+        campsite = CampsiteModel.find_by_name(name)
+        if campsite:
+            campsite.delete()
+            return {"message": "campsite deleted"}
+        return {"message": "campsite not found"}, 404
+
+    def put(self, name):
+
+        data = Campsite.parser.parse_args()
+
+        campsite = CampsiteModel.find_by_name(name)
+
+        if campsite:
+            try:
+                campsite.lat = data["lat"]
+                campsite.lng = data["lng"]
+                campsite.weather_url = data["weather_url"]
+            except:
+                return {"message": "an error occurred updating the campsite"}, 500
+        else:
+            try:
+                campsite = CampsiteModel(
+                    name, data["lat"], data["lng"], data["weather_url"]
+                )  # **data would expand to data["price"], data["store_id"]
+                campsite.upsert()
+            except:
+                return {"message": "an error occured inserting the campsite"}, 500
+        campsite.upsert()
+        return campsite.json()
+
+
+class CampsiteList(Resource):
+    def get(self):
+        return {"campsites": [campsite.json() for campsite in CampsiteModel.query.all()]}
+
+        # connection = sqlite3.connect("data.db")
+        # cursor = connection.cursor()
+
+        # query = "SELECT * FROM campsites"
+        # result = cursor.execute(query)
+
+        # campsites = []
+
+        # for row in result:
+        #     campsites.append({"name": row[1], "price": row[2]})
+        # connection.close()
+
+        # return {"campsites": campsites}
